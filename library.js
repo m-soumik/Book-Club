12d2e9084a11344b1ace2c0dc59f073e7dd4c21c
SOUMIK MUKHOPADHYAY <85982129+m-soumik@users.noreply.github.com>
2022-08-20 20:19:01 +0530

    Update library.js

--- library.js
+++ library.js
@@ -1,198 +1,163 @@
-// Soumik Mukhopadhyay(1928127)
-let TEST_DATA = [
+// Write your code here!
+//Developed by Soumik Mukhopadhay (1928127)
+document.getElementById("logged-in-user-name").innerHTML = "No user logged in";
+
+let arr = [
     {
-        id: 1,
-        Book: 'Book1',
-        Author: 'Author1',
-        Lender: 'UserC',
-        Borrower: 'UserB',
+        id:"1",
+        book:"Book1",
+        Author:"Author1",
+        Lender:"UserC",
+        Borrower:"UserB",
+        Actions:"",
     },
     {
-        id: 2,
-        Book: 'Book2',
-        Author: 'Author2',
-        Lender: 'UserC',
-        Borrower: null,
+        id:"2",
+        book:"Book2",
+        Author:"Author2",
+        Lender:"UserC",
+        Borrower:"",
+        Actions:"",
     },
     {
-        id: 3,
-        Book: 'Book3',
-        Author: 'Author3',
-        Lender: 'UserD',
-        Borrower: 'UserC',
+        id:"3",
+        book:"Book3",
+        Author:"Author3",
+        Lender:"UserD",
+        Borrower:"UserC",
+        Actions:"",
     },
     {
-        id: 4,
-        Book: 'Book4',
-        Author: 'Author4',
-        Lender: 'UserA',
-        Borrower: null,
+        id:"4",
+        book:"Book4",
+        Author:"Author4",
+        Lender:"UserA",
+        Borrower:"",
+        Actions:"",
     },
     {
-        id: 5,
-        Book: 'Book5',
-        Author: 'Author5',
-        Lender: 'UserA',
-        Borrower: null,
+        id:"5",
+        book:"Book5",
+        Author:"Author5",
+        Lender:"UserA",
+        Borrower:"",
+        Actions:"",
     },
     {
-        id: 6,
-        Book: 'Book6',
-        Author: 'Author6',
-        Lender: 'UserB',
-        Borrower: 'UserA',
+        id:"6",
+        book:"Book6",
+        Author:"Author6",
+        Lender:"UserB",
+        Borrower:"UserA",
+        Actions:"",
     }
 ]
 
+let count=0;
 
-
-let isLoggedIn = false;
-let totalBookCount = TEST_DATA.length;
-let table = document.querySelector('#info-table');
-table.appendChild(document.createElement('tbody'));
-
-const clear_table = () => {
-    let table = document.querySelectorAll('#info-table tbody tr');
-    table.forEach(r => (r.remove()));
-}
-
-const createRow = (r) => {
-    const tr = document.createElement('tr');
-    const idTh = document.createElement('th');
-    const titleTh = document.createElement('th');
-    const authorTh = document.createElement('th');
-    const lenderTh = document.createElement('th');
-    const borrowerTh = document.createElement('th');
-    const actionTh = document.createElement('th');
-    idTh.innerText = r.id;
-    titleTh.innerHTML = r.Book;
-    authorTh.innerHTML = r.Author;
-    lenderTh.innerText = r.Lender;
-    borrowerTh.innerHTML = r.Borrower || '--';
-    if(r.Action) {
-        actionTh.innerHTML = r.Action;
-    } else {
-        actionTh.innerHTML = '--';
+function changeLoggedInUser(){
+    let logIn = document.getElementById("logged-user");
+    let userName = logIn.value;
+    if(userName === ""){
+        alert("LOGIN FAILED !!\n(Username is null)");
     }
-    tr.appendChild(idTh);
-    tr.appendChild(titleTh);
-    tr.appendChild(authorTh);
-    tr.appendChild(lenderTh);
-    tr.appendChild(borrowerTh);
-    tr.appendChild(actionTh);
-    return tr;
-}
-
-const add_data = (data) => {
-    let table = document.querySelector('#info-table tbody');
-    data.forEach((d) => {
-        const tr = createRow(d);
-        table.appendChild(tr);
-    })
-}
-
-const displayData = () => {
-    clear_table();
-    add_data(TEST_DATA);
-}
-
-displayData();
-
-let temo;
-
-const addBook = () => {
-    const newDataRow = document.querySelectorAll('#info-table .newData th');
-    console.log(newDataRow)
-    temo = newDataRow;
-    const newData = {
-        id: newDataRow[0].innerText,
-        Book: newDataRow[1].lastChild.value,
-        Author: newDataRow[2].lastChild.value,
-        Lender: newDataRow[3].innerText,
-        Borrower: null
-    }
-    TEST_DATA.push(newData);
-    totalBookCount++;
-    displayData();
-    document.querySelector('#logged-user').value = '';
-    isLoggedIn = false;
-    document.querySelector('#logged-in-user-name').innerText = 'No user logged in';
-}
-
-const createData = (LenderName) => {
-    const id = totalBookCount + 1;
-    const title = '<input type="text" placeholder="Title" required>';
-    const author = '<input type="text" placeholder="Author" required>';
-    const Lender = LenderName;
-    const borrower = null;
-    const action = '<button onclick="addBook()">Add Book</button>';
-    const newData = {
-        id: id,
-        Book: title,
-        Author: author,
-        Lender: Lender,
-        Borrower: borrower,
-        Action: action
-    }
-    return newData;
-}
-
-const changeLoggedInUser = () => {
-    const p = document.querySelector('#logged-in-user-name');
-    const u = document.querySelector('#logged-user').value;
-    if(u.trim().length > 0){
-        const actionData = [];
-        for(let d of TEST_DATA){
-            console.log(d);
-            const data = {
-                id: d.id,
-                Book: d.Book,
-                Author: d.Author,
-                Lender: d.Lender,
-                Borrower: d.Borrower,
-                Action: null
-            }
-            if(d.Lender === u){
-                data.Action = null;
-            } else {
-                if(d.Borrower === u){
-                    data.Action = `<button id=${data.id} onclick="returnBook(${data.id})">Return Book</button>`
-                } else if (d.Borrower == null){
-                    data.Action = `<button id=${data.id} onclick="borrowBook(${data.id})">Borrow Book</button>`
-                }
-            }
-            actionData.push(data);
+    else{
+        let obj = arr.find(key => key.Lender === userName);
+        if(obj){
+            count++;
+            document.getElementById("logged-in-user-name").innerHTML = "Logged in user: "+userName;
+            addActionButton(userName);
+            addBookCol(userName,count);
         }
-        clear_table();
-        add_data(actionData);
-        let table = document.querySelector('#info-table tbody');
-        p.innerText = `Logged in user: ${u}`;
-        isLoggedIn = true;
-        const rowData = createData(u);
-        const tr = createRow(rowData);
-        tr.classList.add('newData');
-        table.appendChild(tr);
-    } else {
-        displayData();
-        p.innerText = 'No user logged in';
-        isLoggedIn = false;
+        else
+            alert("LOGIN FAILED !!\n(User doesnt exists)");
+    }  
+}
+
+function populateTable(){   
+    let html = "<thead><tr><th>Id</th><th>Title</th><th>Author</th><th>Lender</th><th>Borrower</th><th>Action</th></tr></thead>";
+    
+    for(let i=0;i<arr.length;i++){
+        html+="<tr>";
+        html+="<td>"+arr[i].id+"</td>";
+        html+="<td>"+arr[i].book+"</td>";
+        html+="<td>"+arr[i].Author+"</td>";
+        html+="<td>"+arr[i].Lender+"</td>";
+        html+="<td>"+arr[i].Borrower+"</td>";
+        html+="<td>"+arr[i].Actions+"</td>";
+        html+="</tr>";
+        }
+        document.getElementById("info-table").innerHTML = html;
+}
+
+populateTable();
+
+let table = document.getElementById("info-table");
+function addBookCol(name,count){
+    if(count!=1) table.deleteRow(arr.length+1);
+    var row = table.insertRow(arr.length+1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    cell1.innerHTML = arr.length+1;
+    cell4.innerHTML = name;
+
+    cell2.innerHTML = `<input id=\"bookInput\" type=\"text\"/ placeholder=\"bookname\">`;
+    cell3.innerHTML = `<input id=\"authorInput\" type=\"text\"/ placeholder=\"authorname\">`;
+
+    cell6.innerHTML = `<button onclick=\"newBookAdd('${name}','${count}')\" class=\"btn btn-warning\">Add Book</button>`;
+}
+
+function newBookAdd(name,count){
+    let book = document.getElementById('bookInput').value;
+    let author = document.getElementById('authorInput').value;
+    
+    if(book!="" && author!=""){
+        console.log(book);
+        var row = table.insertRow(arr.length+1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML = ++arr.length;
+        
+        cell2.innerHTML = document.getElementById('bookInput').value;
+        cell3.innerHTML = document.getElementById('authorInput').value;
+        cell4.innerHTML = name;
+        addBookCol(name,arr.length);     
+    }
+    else{
+        alert("Book Name and Author Name cannot be empty !!");
     }
 }
 
-
-const returnBook = (id) => {
-    console.log(id);
-    TEST_DATA[id-1].Borrower = null;
-    displayData();
-    document.querySelector('#logged-user').value = '';
-    isLoggedIn = false;
-    document.querySelector('#logged-in-user-name').innerText = 'No user logged in';
+function addActionButton(name){
+    let table = document.getElementById("info-table");
+    for (var i = 1, row; row = table.rows[i]; i++){
+        let lender = row.cells[3].innerHTML;
+        let borrower = row.cells[4].innerHTML;
+        if(lender!=name){
+            if(borrower=="") row.cells[5].innerHTML=`<button onclick=\"borrowBook('${i}','${name}')\" class=\"btn btn-success\">Borrow</button>`;
+            else if(borrower===name) row.cells[5].innerHTML=`<button onclick=\"returnBook('${i}','${name}')\" class=\"btn btn-danger\">Return</button>`;
+            else row.cells[5].innerHTML="";
+        }
+        else row.cells[5].innerHTML="";
+    }
 }
 
-const borrowBook = (id) => {
-    TEST_DATA[id-1].Borrower = document.querySelector('#logged-user').value;
-    displayData();
-    document.querySelector('#logged-user').value = '';
-    isLoggedIn = false;
-    document.querySelector('#logged-in-user-name').innerText = 'No user logged in';
+function borrowBook(i,name){
+    let table = document.getElementById("info-table");
+    table.rows[i].cells[4].innerHTML=name;
+    table.rows[i].cells[5].innerHTML=`<button onclick=\"returnBook('${i}','${name}')\" class=\"btn btn-danger\">Return</button>`;
+}
+
+function returnBook(i,name){
+    let table = document.getElementById("info-table");
+    table.rows[i].cells[4].innerHTML="";
+    table.rows[i].cells[5].innerHTML=`<button onclick=\"borrowBook('${i}','${name}')\" class=\"btn btn-success\">Borrow</button>`;
 }
